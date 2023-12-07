5f8bc37cea7e1383af3bb552639bedc62389d3e3
Waiganjo <waiganjothiongo12@gmail.com>
2023-12-07 12:20:13 -0800

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *a;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+		{
+			head = head->prev;
+		}
+	}
+	while ((a = head) != NULL)
+	{
+		head = head->next;
+		free(a);
+	}
+}
