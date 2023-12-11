0efd148306fcba4c957ecb05a3022869a3273d45
Waiganjo <waiganjothiongo12@gmail.com>
2023-12-11 05:41:16 -0800

    Task 3

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a;
+	int b;
+
+	a = 0;
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+			{
+				a++;
+				break;
+			}
+			else if (accept[b + 1] == '\0')
+			{
+				return (a);
+			}
+		}
+		s++;
+	}
+	return (a);
+}
