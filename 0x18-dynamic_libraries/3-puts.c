94967878f36d795f77c2f23439b47b106594a0af
Waiganjo <waiganjothiongo12@gmail.com>
2023-12-11 10:24:34 -0800

    Task 0

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,10 +1,12 @@
 #include "main.h"
-#include<stdio.h>
 /**
  * _puts - A void
  * @str: A character
  */
 void _puts(char *str)
+{
+}
+#include<stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
